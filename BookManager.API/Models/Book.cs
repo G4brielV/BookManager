1cdc7d7040b743bda9f236d05eab3604a723b2ba
Gabriel Vinícius <gabrielvto18@gmail.com>
2026-06-03 00:31:30 -0300

    models

---
+++ BookManager.API/Models/Book.cs
@@ -0,0 +1,12 @@
+namespace BookManager.API.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public DateTime PublishDate { get; set; }
+    
+    public int UserId { get; set; }
+    public User? User { get; set; }
+}
