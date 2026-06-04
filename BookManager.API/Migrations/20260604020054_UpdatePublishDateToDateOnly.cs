47b2add0779bf3ac5cbfff7d82d6c92d867e897c
Gabriel Vinícius <gabrielvto18@gmail.com>
2026-06-03 23:20:35 -0300

    Mudando de timestamp para data no banco com migrations

---
+++ BookManager.API/Migrations/20260604020054_UpdatePublishDateToDateOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePublishDateToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "publish_date",
+                table: "books",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "publish_date",
+                table: "books",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
